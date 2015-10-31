26b8644b8106faa22604ac5e39f7abcd4af9cebc
Adrian Norberto Marino <adrianmarino@gmail.com>
2015-10-31 17:26:50 -0300

    support blocks

--- example.rb
+++ example.rb
@@ -10,17 +10,22 @@ class Example
     "instance method 'a_a'"
   end
 
+  def a_a_a
+    "instance method 'a_a' (block: #{yield})"
+  end
+
   def self.a
     "class method 'a'"
   end
 end
 
-block = ->(method, *args) { puts "Wrap #{method.call(*args)}" }
-Example.wrap_method(pattern: /^a$/, &block)
-Example.wrap_instance_method(pattern: /^a/, &block)
+wrapper = ->(method, *args, &block) { puts "Wrap #{method.call(*args, &block)}" }
+Example.wrap_method(pattern: /^a$/, &wrapper)
+Example.wrap_instance_method(pattern: /^a/, &wrapper)
 
 example = Example.new
 example.a
 example.a_a
+example.a_a_a { "result" }
 
 Example.a
--- method_wrapper.rb
+++ method_wrapper.rb
@@ -7,17 +7,17 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 class Class
-  def wrap_method(pattern:, &block)
+  def wrap_method(pattern:, &wrapper)
     methods.grep(pattern).each do |name|
       method = method(name)
-      define_singleton_method(name) { |*args| block.call(method, *args) }
+      define_singleton_method(name) { |*args, &block| wrapper.call(method, *args, &block) }
     end
   end
 
-  def wrap_instance_method(pattern:, &block)
+  def wrap_instance_method(pattern:, &wrapper)
     instance_methods.grep(pattern).each do |name|
       method = instance_method(name)
-      define_method(name) { |*args| block.call(method.bind(self), *args) }
+      define_method(name) { |*args, &block| wrapper.call(method.bind(self), *args, &block) }
     end
   end
 end
